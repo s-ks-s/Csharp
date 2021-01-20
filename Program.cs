35986560fae5d61decedfb651e75deed23f7245b
s-ks-s <65196027+s-ks-s@users.noreply.github.com>
2021-01-20 23:21:02 +0300

    Snake

--- Program.cs
+++ Program.cs
@@ -1,338 +1,117 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Runtime.ConstrainedExecution;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
-namespace tic_tac_toe
+namespace Snake_2._0
 {
     class Program
     {
-        public static string[,] TheTableArray = new string[9, 11];
-        public static string existingNumbers = "";
-        public static bool final = false;
-
-        public static void TableCreation()
+        public static string direction = "";
+        public static bool isSnake = false;
+        public static int row { get; set; } = 25;
+        public static int column { get; set; } = 50;
+        //кортеж
+        public static (int, int) GetAim()
         {
-            #region Description
-            //Создадим массив, чтобы цифры можно было редактировать обращаясь к элементу массива ( массив со значениями string)
-            //заполним массив циклом
-            /*
-            ***|***|*** row0
-            *1*|*2*|*3* row1
-            ---|---|--- row2
-            ***|***|*** row3
-            *4*|*5*|*6* row4
-            ---|---|--- row5
-            ***|***|*** row6
-            *7*|*8*|*9* row7
-            ***|***|*** row8
-            */
-            #endregion
-
-            #region Заполнение массива
-            //Вынесла массив в общий доступ, т.к. разные методы должны к нему обращаться
-            //string[,] TheTableArray = new string[9, 11];
-            int number = 1;
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 11; j++)
-                {
-                    if (j == 3 || j == 7)
-                    {
-                        TheTableArray[i, j] = "|";
-                    }
-                    else
-                    {
-                        if (i == 2 || i == 5)
-                        {
-                            TheTableArray[i, j] = "_";
-                        }
-                        else
-                        {
-                            if ((i == 1 || i == 4 || i == 7) && (j == 1 || j == 5 || j == 9))
-                            {
-                                TheTableArray[i, j] = Convert.ToString(number);
-                                number++;
-                            }
-                            else
-                                TheTableArray[i, j] = " ";
-                        }
-                    }
-                }
-            }
-            #endregion
-
+            var result = (0,0);
+            Random rnd = new Random();
+            result.Item1 = rnd.Next(1, (column-1));
+            result.Item2 = rnd.Next(1, (row-1));
+            return result;
         }
 
-        public static void TablePopulation()
-        { 
-            #region Вывод на экран (моя версия)
-            for (int i = 0; i < 9; i++)
+        public static void Show(int aimX, int aimY,int row,int column, List<SnakeCell> snakeList)
+        {
+            for (int row1 = 0;row1<=row;row1++)
             {
-                for (int j = 0; j < 11; j++)
+                for (int column1 = 0;column1<=column;column1++)
                 {
-                    if (j == 3 || j == 7)
+                    if (row1 == 0 || row1 == row)
                     {
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.Write(TheTableArray[i,j]);
+                        Console.Write("-");
                     }
                     else
                     {
-                        if (i == 2 || i == 5)
+                        if (column1 == 0 || column1 == column)
                         {
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            Console.Write(TheTableArray[i, j]);
+                            Console.Write("|");
                         }
                         else
                         {
-                            if ((i == 1 || i == 4 || i == 7) && (j == 1 || j == 5 || j == 9))
+                            isSnake = false;
+                            snakeList.ForEach(delegate (SnakeCell snakecell)
                             {
-                                if (TheTableArray[i, j] == "X")
-                                { Console.ForegroundColor = ConsoleColor.Yellow; }
-                                else if (TheTableArray[i, j] == "O")
-                                { Console.ForegroundColor = ConsoleColor.Red; }
+                                if (row1 == snakecell.y && column1 == snakecell.x)
+                                {
+                                    Console.Write("s");
+                                    isSnake = true;
+                                }
+                            });
+                            if (isSnake == false)
+                            {
+                                if (column1 == aimX && row1 == aimY)
+                                    Console.Write("O");
                                 else
-                                { Console.ForegroundColor = ConsoleColor.DarkGray; }
-                                Console.Write(TheTableArray[i, j]);
+                                Console.Write(" ");
                             }
-                            else
-                                Console.Write(TheTableArray[i, j]);
                         }
                     }
                 }
                 Console.WriteLine();
             }
-            #endregion
-
-            #region  Вывод на экран(комментарии и доп сценарии)
-        /*
-        //Определенную сложность может представлять перебор многомерного массива. 
-        //Прежде всего надо учитывать, что длина такого массива - это совокупное количество элементов.
-        //И цикл foreach выводит все элементы массива в строку =(
-        //если мы хотим отдельно пробежаться по каждой строке в таблице - надо получить количество элементов в размерности. 
-        //у каждого массива есть метод GetUpperBound(dimension), который возвращает индекс последнего элемента в определенной размерности. 
-        //В двухмерном массиве первая размерность (с индексом 0) по сути это и есть таблица. 
-        //И с помощью выражения mas.GetUpperBound(0) + 1 можно получить количество строк таблицы, представленной двухмерным массивом.
-        //А через mas.Length / rows можно получить количество элементов в каждой строке
-        int rows = mas.GetUpperBound(0) + 1;
-        int columns = mas.Length / rows;
-        // или так
-        // int columns = mas.GetUpperBound(1) + 1;
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                Console.Write($"{mas[i, j]} \t");
-            }
-            Console.WriteLine();
-        }
-        */
-        #endregion
-         }
-
-        public static void Player1()
-        {
-            string chosenString = "";
-            int chosenNumber = 0;
-            bool success = false;
-            bool exist = false;
-            do
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Player 1 (X): Enter the number of cell");
-                chosenString = Console.ReadLine();
-                //проверка на ошибку ввода не цифры, а буквы или других символов
-                success = int.TryParse(chosenString, out chosenNumber);
-                //проверка, что такая цифра ещё не занята
-                exist = existingNumbers.Contains(chosenString);
-                if (success == false)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Fill in only Number. Try again!");
-                }
-                else
-                {
-                    if (exist == true)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("This cell has been already chosen. Try again!");
-                    }
-                    else
-                        existingNumbers += chosenNumber;
-                }
-            }
-            while (success == false || exist == true);
-
-            // Если введенное число совпало со стринговой записью числа, то замени на X
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 11; j++)
-                {
-                    TheTableArray[i, j] = TheTableArray[i, j].Replace(chosenString, "X");
-                }
-            }
 
         }
-
-        public static void Player2()
-        {
-            string chosenString = "";
-            int chosenNumber = 0;
-            bool success = false;
-            bool exist = false;
-            do
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Player 2 (O): Enter the number of cell");
-                chosenString = Console.ReadLine();
-                //проверка на ошибку ввода не цифры, а буквы или других символов
-                success = int.TryParse(chosenString, out chosenNumber);
-                //проверка, что такая цифра ещё не занята
-                exist = existingNumbers.Contains(chosenString);
-                if (success == false)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Fill in only Number. Try again!");
-                }
-                else
-                {
-                    if (exist == true)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("This cell has been already chosen. Try again!");
-                    }
-                    else
-                        existingNumbers += chosenNumber;
-                }
-            }
-            while (success == false || exist == true);
-
-            // Если введенное число совпало со стринговой записью числа, то замени на O
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 11; j++)
-                {
-                    TheTableArray[i, j] = TheTableArray[i, j].Replace(chosenString, "O");
-                }
-            }
-
-        }
-
-        public static void Winner()
-        {
-            
-            int winnercounter = 0;
-            string Congrats = "";
-            string winner = "X";
-
-            for (int i = 0; i<2; i++)
-            {
-                bool line1 = (winner == TheTableArray[1, 1] && TheTableArray[1, 1] == TheTableArray[1, 5] && TheTableArray[1, 5] == TheTableArray[1, 9]);
-                bool line2 = (winner == TheTableArray[4, 1] && TheTableArray[4, 1] == TheTableArray[4, 5] && TheTableArray[4, 5] == TheTableArray[4, 9]);
-                bool line3 = (winner == TheTableArray[7, 1] && TheTableArray[7, 1] == TheTableArray[7, 5] && TheTableArray[7, 5] == TheTableArray[7, 9]);
-                bool diagonal1 = (winner == TheTableArray[1, 1] && TheTableArray[1, 1] == TheTableArray[4, 5] && TheTableArray[4, 5] == TheTableArray[7, 9]);
-                bool diagonal2 = (winner == TheTableArray[1, 9] && TheTableArray[1, 9] == TheTableArray[4, 5] && TheTableArray[4, 5] == TheTableArray[7, 1]);
-                bool column1 = (winner == TheTableArray[1, 1] && TheTableArray[1, 1] == TheTableArray[4, 1] && TheTableArray[4, 1] == TheTableArray[7, 1]);
-                bool column2 = (winner == TheTableArray[1, 5] && TheTableArray[1, 5] == TheTableArray[4, 5] && TheTableArray[4, 5] == TheTableArray[7, 5]);
-                bool column3 = (winner == TheTableArray[1, 9] && TheTableArray[1, 9] == TheTableArray[4, 9] && TheTableArray[4, 9] == TheTableArray[7, 9]);
-                final = (line1 || line2 || line3 || diagonal1 || diagonal2 || column1 || column2 || column3);
-                if (final == true)
-                {
-                    Congrats = winner;
-                    winnercounter++;
-                }
-                winner = "O";
-            }
-
-            if (winnercounter == 2)
-            {
-                final = true;
-                Console.WriteLine("DRAW!");
-            }
-            else if (winnercounter == 1)
-            {
-                final = true;
-                if (Congrats == "X")
-                    Console.WriteLine("The winner is Player 1");
-                else
-                    Console.WriteLine("The winner is Player 2");
-            }
-            else if (winnercounter == 0 && existingNumbers.Length == 9)
-            {
-                final = true;
-                Console.WriteLine("Draw!");
-            }
-        }
-
-
         static void Main(string[] args)
         {
-            string reset;
-            do
+            //SnakeCell first = new SnakeCell();
+            Snake snake = new Snake(10, 10,"null","right");
+
+            int num = 0;
+            var aim = GetAim();
+            int aimX = aim.Item1;
+            int aimY = aim.Item2;
+            
+            Show(aimX,aimY,row,column,snake.snakeList);
+            while (num >= 0)
             {
-            reset = "";
-            Array.Clear(TheTableArray,9,11);
-            existingNumbers = "";
-            final = false;
-            Console.Clear();
-            Console.WriteLine("Let's play a tic tac toe!");
-                //Создание массива и заполнение его дефолтными значениями
-                TableCreation();
-                //вывод на экран поля и цифр с названиями ячеек
-                TablePopulation();
+                Console.WriteLine("Press the button");
+                ConsoleKeyInfo pressButton = Console.ReadKey();
+                snake.MoveSnake(pressButton);
 
-                #region Image
-                /*
-                ***|***|*** row0
-                *1*|*2*|*3* row1
-                ---|---|--- row2
-                ***|***|*** row3
-                *4*|*5*|*6* row4
-                ---|---|--- row5
-                ***|***|*** row6
-                *7*|*8*|*9* row7
-                ***|***|*** row8
-
-                1 - 1,1
-                2 - 1,5
-                3 - 1,9
-                4 - 4,1
-                5 - 4,5
-                6 - 4,9
-                7 - 7,1
-                8 - 7,5
-                9 - 7,9
-                 */
-                #endregion
-
-                do
+                if (aimX == snake.snakeList.ElementAt(0).x && aimY == snake.snakeList.ElementAt(0).y)
                 {
-                    //цикл запроса хода игрока1
-                    Player1();
-                    //Стираем всё
-                    Console.Clear();
-                    //Вывод новой таблицы с обновленными данными
-                    TablePopulation();
-                    //цикл запроса хода игрока2
-                    //проверка, что не все ячейки заняты
-                    if (existingNumbers.Length != 9)
+                    if(snake.snakeList.ElementAt(snake.snakeList.Count - 1).direction == "right")
                     {
-                        Player2();
-                        Console.Clear();
-                        //Вывод новой таблицы с обновленными данными
-                        TablePopulation();
+                        snake.AddNewCell(snake.snakeList.ElementAt(snake.snakeList.Count - 1).x - 1, snake.snakeList.ElementAt(snake.snakeList.Count - 1).y, "null", "null");
                     }
-                    //проверка победы
-                    Winner();
+                    if(snake.snakeList.ElementAt(snake.snakeList.Count - 1).direction == "left")
+                    {
+                        snake.AddNewCell(snake.snakeList.ElementAt(snake.snakeList.Count - 1).x + 1, snake.snakeList.ElementAt(snake.snakeList.Count - 1).y, "null", "null");
+                    }
+                    if(snake.snakeList.ElementAt(snake.snakeList.Count - 1).direction == "up")
+                    {
+                        snake.AddNewCell(snake.snakeList.ElementAt(snake.snakeList.Count - 1).x, snake.snakeList.ElementAt(snake.snakeList.Count - 1).y+1, "null", "null");
+                    }
+                    if(snake.snakeList.ElementAt(snake.snakeList.Count - 1).direction == "down")
+                    {
+                        snake.AddNewCell(snake.snakeList.ElementAt(snake.snakeList.Count - 1).x, snake.snakeList.ElementAt(snake.snakeList.Count - 1).y-1, "null", "null");
+                    }
+                    aim = GetAim();
+                    aimX = aim.Item1;
+                    aimY = aim.Item2;
                 }
-                while (final == false);
-
-                //Reset
-                Console.WriteLine("To reset game please press Y, other symbols will closed programm");
-                reset = Console.ReadLine();
-            }
-            while (reset == "Y" || reset == "y");
-
+                if(snake.snakeList.ElementAt(0).x == column || snake.snakeList.ElementAt(0).y == row)
+                {
+                    Console.WriteLine("Game over");
+                    break;
+                }
+                snake.UpdateSnake();
+                Console.Clear();
+                Show(aimX, aimY, row, column, snake.snakeList);
+                num++;
+            }            
+            Console.ReadKey();
         }
     }
- }
+}
